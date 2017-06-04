22a5fa6f462a655fa374eb4a2ca5f556aee2b470
Benni <webmaster@freiken-douhl.de>
2017-06-04 15:55:41 +0200

    Enabled syncing to CouchDB server

--- src/client/app/shared/DB.js
+++ src/client/app/shared/DB.js
@@ -3,11 +3,29 @@ import PouchDBFind from 'pouchdb-find';
 
 PouchDB.plugin(PouchDBFind);
 
-const challenges = new PouchDB('challenges');
-const quests = new PouchDB('quests');
+const exportedDbs = {};
+const dbs = ['challenges', 'quests'];
 
-quests.createIndex({ index: { fields: ['challenge'] } });
+dbs.forEach(db =>
+{
+  const remoteDB = new PouchDB('https://schnitzel.freiken-douhl.de/db/' + db);
+  const localDB = exportedDbs[db] = new PouchDB(db);
 
-export default { challenges, quests };
+  localDB.sync(remoteDB)
+    .on('complete', () =>
+    {
+      console.log('Synced [' + db + '] database.');
+    })
+    .on('error', (err) =>
+    {
+      console.log('Failed to sync [' + db + '] database.');
+      console.error(err);
+    })
+  ;
+});
 
-window.PouchDB = PouchDB;
+exportedDbs.quests.createIndex({ index: { fields: ['challenge'] } });
+
+export default exportedDbs;
+
+// window.PouchDB = PouchDB;
